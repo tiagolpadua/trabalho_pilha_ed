4929662177497b3ecd66f8c09f17d1d13e27f336
Tiago <Tiago@pc-tim-w8>
2012-11-27 23:21:26 -0200

    Commit inicial

---
+++ src/main/java/gov/unb/cic/ed/trabalhopilhaed/App.java
@@ -0,0 +1,13 @@
+package gov.unb.cic.ed.trabalhopilhaed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
